9a8d8ca1a7c25adff9165a07f7196a32404ea5fa
Aynal-dev <aynaldevgen@gmail.com>
2026-06-21 11:05:44 +0600

    Add MERN auth backend with register/login API

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
+dotenv.config();
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the server!' });
 });
+
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
